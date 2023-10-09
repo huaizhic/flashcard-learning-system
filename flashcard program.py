dc0eabde978292f16d936306cfb9515215b3f56d
huaizhic <huaizhihuaizhi@gmail.com>
2023-10-09 19:09:07 +0800

    Major Update
    Sorted by pre and post recess week content. Add word to database function now fully functional. Overall program writing currently has a few redundancies, in the future can write it OOP style to make it cleaner

--- flashcard program.py
+++ flashcard program.py
@@ -8,9 +8,20 @@ def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
 def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
 
 
-def readingsystem():
-    with open('database.txt') as f:
-        print("Reading database.txt...")
+def readingsystem1():
+    with open('databaseWk1-7.txt') as f:
+        print("Reading databaseWk1-7.txt...")
+        data = f.read()
+
+    database = json.loads(data)
+    print("database.txt successfully read!")
+    # print(database)
+    return database
+
+
+def readingsystem2():
+    with open('databaseWk8-13.txt') as f:
+        print("Reading databaseWk8-13.txt...")
         data = f.read()
 
     database = json.loads(data)
@@ -67,7 +78,7 @@ def flashcardsystem(database):
             print(" It is (", answer, ")")
 
 
-def addtoDatabase():
+def addtoDatabase1():
     print("English word: (To go back menu, enter 0)")
     userEnglishInput = input()
     if userEnglishInput == "0":
@@ -78,7 +89,54 @@ def addtoDatabase():
         if userMalayInput == "0":
             menu()
         else:
-            with open('database.txt', 'a') as f:
+            with open('databaseWk1-7.txt') as f:
+                data = f.read()                # string
+            dataDict = json.loads(data)    # from string to dictionary
+            dataDict.update({userEnglishInput: userMalayInput})
+            # from dictionary to JSON
+            dataJSON = json.dumps(dataDict, indent=2)
+            dataString = str(dataJSON)  # from JSON to string
+            with open('databaseWk1-7.txt', "w") as f:
+                f.write(dataString)
+            f.close()
+            prGreen("Addition successful!")
+            # print("Remember to delete the } at the end")
+
+
+def addtoDatabase2():
+    print("English word: (To go back menu, enter 0)")
+    userEnglishInput = input()
+    if userEnglishInput == "0":
+        menu()
+    else:
+        print("Malay Equivalent: (To go back menu, enter 0)")
+        userMalayInput = input()
+        if userMalayInput == "0":
+            menu()
+        else:
+            with open('databaseWk8-13.txt') as f:
+                data = f.read()                # exact string
+            dataDict = json.loads(data)    # from string to dictionary
+            dataDict.update({userEnglishInput: userMalayInput})
+            # from dictionary to JSON
+            dataJSON = json.dumps(dataDict, indent=2)
+            dataString = str(dataJSON)  # from JSON to string
+            with open('databaseWk8-13.txt', "w") as f:
+                f.write(dataString)
+            f.close()
+            prGreen("Addition successful!")
+            # print("Remember to delete the } at the end")
+    """print("English word: (To go back menu, enter 0)")
+    userEnglishInput = input()
+    if userEnglishInput == "0":
+        menu()
+    else:
+        print("Malay Equivalent: (To go back menu, enter 0)")
+        userMalayInput = input()
+        if userMalayInput == "0":
+            menu()
+        else:
+            with open('databaseWk8-13.txt', 'a') as f:
                 # f.write("\b") - backspace cmd doesn't work here
                 # move cursor one byte (character) backwards
                 # f.seek(-1, io.SEEK_END) this parameter combi does not work due to some reason
@@ -95,7 +153,7 @@ def addtoDatabase():
                 f.write("\"")
                 f.write("}")
             prGreen("Addition successful!")
-            print("Remember to delete the } at the end")
+            print("Remember to delete the } at the end")"""
 
 
 def menu():
@@ -104,19 +162,26 @@ def menu():
 flashcard learning system
 -------------------------""")
     print("Select the following options:")
-    print("1: Do the questions")
-    print("2: Add to vocublary database (beta, have to manually delete the duplicate '}'s at the end of each new addition to follow JSON format)")
-    print("3: Search word (coming soon)")
+    print("1: Do the questions (Wk 1-7)")
+    print("2: Do the questions (Wk 8-13)")
+    print("3: Add to vocublary database (Wk1-7)")
+    print("4: Add to vocublary database (Wk8-13)")
+    print("5: Search word (coming soon)")
     print("=========================")
     userInput = input()
     match(userInput):
         case "1":
-            database = readingsystem()
+            database = readingsystem1()
             flashcardsystem(database)
         case "2":
+            database = readingsystem2()
+            flashcardsystem(database)
+        case "3":
             while True:
-                addtoDatabase()
-
+                addtoDatabase1()
+        case "4":
+            while True:
+                addtoDatabase2()
         case _:
             prRed("Error!")
             print("please select the appropriate options")
